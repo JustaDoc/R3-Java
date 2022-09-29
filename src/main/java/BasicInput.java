2bdffadb44ccad4f1849bf64d91b8c58020817a4
JusaDoc <mrpereirac@gmail.com>
2022-09-29 09:46:53 -0400

    Line 10-17 edit and line 29,22,24-25 edit. Removed return that was on line 26.

--- src/main/java/BasicInput.java
+++ src/main/java/BasicInput.java
@@ -3,19 +3,25 @@ import java.util.Scanner;
 public class BasicInput {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
-        int userInt = 0;
-        double userDouble = 0.0;
+
+
         // TODO Define char and string variables similarly
 
         System.out.println("Enter integer:");
-        userInt = scnr.nextInt();
+        int userInt = scnr.nextInt();
+        System.out.println("Enter double:");
+        double userDouble = scnr.nextDouble();
+        System.out.println("Enter character:");
+        char userChar = scnr.next().charAt(0);
+        System.out.println("Enter string:");
+        String userString = scnr.next();
 
         // TODO (1): Finish reading other items into variables, then output the four values on a single line separated by a space
-
+        System.out.println(userInt+" "+userDouble+" "+userChar+" "+userString);
         // TODO (2): Output the four values in reverse
-
+        System.out.println(userString+" "+userChar+" "+userDouble+" "+userInt);
         // TODO (3): Cast the double to an integer, and output that integer
-
-        return;
+        int value = (int) userDouble;
+        System.out.println(userDouble+" cast to an integer is "+value);
     }
 }
